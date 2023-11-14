18b06c5bbd3064faa6e7c6a715994106a7c7c2e2
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-14 11:04:27 -0300

    AT

---
+++ scrollreveal.js
@@ -0,0 +1,66 @@
+ScrollReveal().reveal('.introducao', {
+  origin: 'left',
+  distance: '500px',
+  duration: 1000,
+  reset: false
+});
+ScrollReveal().reveal('#imagemVelhos', {
+  origin: 'left',
+  distance: '700px',
+  duration: 1000,
+  reset: false
+});
+ScrollReveal().reveal('#textoimagemVelhos', {
+  origin: 'right',
+  distance: '700px',
+  duration: 1000,
+  reset: false
+});
+ScrollReveal().reveal('.imagemVelho', {
+  scale: 0.5,
+  duration: 1000,
+  reset: false
+});
+ScrollReveal().reveal('.textin', {
+  origin: 'right',
+  distance: '700px',
+  duration: 1000,
+  reset: false
+});
+ScrollReveal().reveal('#depoimentosImagens', {
+  origin: 'bottom',
+  distance: '700px',
+  duration: 1000,
+  reset: false
+});
+ScrollReveal().reveal('#textoDepoimentos', {
+  duration: 1000,
+  reset: false
+});
+ScrollReveal().reveal('.rodape', {
+  origin: 'bottom',
+  distance: '1000px',
+  duration: 1000,
+  reset: false
+});
+
+ScrollReveal().reveal('#cinza', {
+  scale: 0.5,
+  duration: 1000,
+  reset: false
+});
+
+
+ScrollReveal().reveal('.textinhoContato', {
+  origin: 'left',
+  distance: '1000px',
+  duration: 1000,
+  reset: false
+});
+ScrollReveal().reveal('.input-sobre', {
+  origin: 'right',
+  distance: '1000px',
+  duration: 1000,
+  reset: false
+});
+
